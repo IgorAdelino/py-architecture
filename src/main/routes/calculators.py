c13735f954747e81cf194c3c825bcb83f252d294
Igor Adelino <igoradelino@gmail.com>
2025-02-06 19:34:42 -0300

    feat: calc-3-route

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+from src.main.factories.calculator_3_factory import calculator_3_factory
 from src.main.factories.calculator_2_factory import calculator_2_factory
 from src.main.factories.calculator_1_factory import calculator_1_factory
 
@@ -14,5 +15,11 @@ def calculator_1():
 @calc_route_bp.route("/calculator/2", methods=["POST"])
 def calculator_2():
   calculator = calculator_2_factory()
+  response = calculator.calculate(request)
+  return jsonify(response), 200
+
+@calc_route_bp.route("/calculator/3", methods=["POST"])
+def calculator_3():
+  calculator = calculator_3_factory()
   response = calculator.calculate(request)
   return jsonify(response), 200
