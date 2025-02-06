b9f5edf117399c7b9bb7f96bc42e91188bd0c353
Igor Adelino <igoradelino@gmail.com>
2025-02-06 16:59:54 -0300

    feat:  interfaces

--- src/calculators/calculator_2_test.py
+++ src/calculators/calculator_2_test.py
@@ -1,14 +1,17 @@
 from typing import Dict
 from .calculator_2 import Calculator2
 from pytest import raises
+from src.drivers.numpy_hanlder import NumpyHandler
 
 class MockRequest:
   def __init__(self, body: Dict) -> None:
     self.json = body
 
+mock_driver = NumpyHandler()
+
 def test_calculate():
   mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
-  calculator_2 = Calculator2()
+  calculator_2 = Calculator2(driver_handler=mock_driver)
   response =calculator_2.calculate(mock_request)
 
   # Response format
@@ -21,7 +24,7 @@ def test_calculate():
 def test_calculate_with_body_eror():
   mock_request = MockRequest({"number": "a"})
 
-  calculator1 =  Calculator2()
+  calculator1 =  Calculator2(driver_handler = mock_driver)
   with raises(Exception) as excinfo:
     calculator1.calculate(mock_request)
   assert str(excinfo.value) == "Invalid body"
