7849ba2e3de8a59d4ad81ab16a3fbfe481d9fde5
Igor Adelino <igoradelino@gmail.com>
2025-02-06 19:10:48 -0300

    feat: calc-3

---
+++ src/calculators/calculator_3.py
@@ -0,0 +1,56 @@
+from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
+from flask import Request as FlaskRequest
+from typing import Dict, List
+
+class Calculator3(DriverHandlerInterface):
+  def __init__(self, driver_handler: DriverHandlerInterface) -> None:
+    self.__driver_handler = driver_handler
+
+  def calculate(self, request: FlaskRequest)-> Dict:
+    body = request.json
+
+    input_data = self.__validate_body(body)
+
+    variance = self.__calculate_variance(input_data)
+
+    multiplication = self.__calculate_multiplication(input_data)
+
+    self.__verify_results(variance, multiplication)
+
+    formated_response = self.__format_response(multiplication)
+
+    return formated_response
+
+
+  def __validate_body(self, body: Dict) -> float:
+      if "numbers" not in body or not isinstance(body["numbers"], list):
+          raise Exception("Invalid body")
+
+      if not all(isinstance(n, (int, float)) for n in body["numbers"]):
+          raise Exception("Invalid body")
+    
+      input_data = body["numbers"]
+      return input_data
+  
+  def __calculate_variance(self, numbers: List[float]) -> float:
+     variance = self.__driver_handler.variance(numbers)
+     return variance
+  
+  def __calculate_multiplication(self, numbers: List[float]) -> float:
+     multiplication = 1
+     for num in numbers: multiplication *= num
+     
+     return multiplication
+  
+  def __verify_results(self, variance: float, multiplication: float) -> None:
+     if variance < multiplication:
+        raise Exception("Process Failed: Variance is less than multiplication")
+     
+  def __format_response(self, variance: float) -> Dict:
+    return {
+      "data": {
+        "Calculator": 3,
+        "Result": variance,
+        "Success": True
+      }
+    }
