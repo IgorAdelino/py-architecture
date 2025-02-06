c4bc7d564a4d9bbbfcaca39a388e0c32de66bacf
Igor Adelino <igoradelino@gmail.com>
2025-02-06 11:06:56 -0300

    feat: unit tests

---
+++ src/calculators/calculator_1_test.py
@@ -0,0 +1,33 @@
+from .calculator_1 import Calculator1
+from typing import Dict
+from pytest import raises
+
+class MockRequest:
+  def __init__(self, body: Dict)-> None:
+    self.json = body
+
+def test_calculate():
+  mock_request = MockRequest({"number": 1})
+
+  calculator1 =  Calculator1()
+
+  response = calculator1.calculate(mock_request)
+  
+  print(response)
+  # Response format
+  assert "data" in response
+  assert "Calculator" in response['data']
+  assert "result" in response["data"]
+
+  # Response value
+  assert response["data"]["result"] == 14.24740748999224
+  assert response["data"]["Calculator"] == 1
+
+def test_calculate_with_body_eror():
+  mock_request = MockRequest({"number": "a"})
+
+  calculator1 =  Calculator1()
+  with raises(Exception) as excinfo:
+    calculator1.calculate(mock_request)
+  assert str(excinfo.value) == "Invalid body"
+  
